c450b348fb361a3385d9a379fc9b50848f1a3e48
bannedbook <banned.ebook@gmail.com>
2020-06-07 15:25:33 +0800

    update

--- gitupdate.bat
+++ gitupdate.bat
@@ -3,6 +3,6 @@ git pull origin master
 git add -A
 git commit -m "update"
 git push origin master
-git tag -a v2.0.8 -m "release v2.0.8"
+git tag -a v2.0.9 -m "release v2.0.9"
 git push origin --tags
 pause
